704f45913f8b784da56a073d6976834bcbec96a6
sphrilix <maxi.jungwirth@googlemail.com>
2022-11-08 22:29:54 +0100

    renaming to python convention in predict.py

--- predict.py
+++ predict.py
@@ -4,41 +4,43 @@ from tensorflow_model import Code2VecModel
 from config import Config
 
 
-def load_model(config: Config) -> Code2VecModel:
-    model = Code2VecModel(config)
-    model.predict([])
-    return model
+def load_model(conf: Config) -> Code2VecModel:
+    code2vec_model = Code2VecModel(conf)
+    code2vec_model.predict([])
+    return code2vec_model
 
 
-def predict(model: Code2VecModel, ast_path: str) -> None:
-    raw_result = model.predict([ast_path])
+def predict(code2vec_model: Code2VecModel, ast_path: str) -> None:
+    raw_result = code2vec_model.predict([ast_path])
     if raw_result:
         print(parse_result_for_plugin(raw_result[0]))
 
 
 def parse_result_for_plugin(raw_prediction: ModelPredictionResults) -> list:
-    return list(map(list, zip(map(parseTokensToMethodName, raw_prediction.topk_predicted_words), 
-        raw_prediction.topk_predicted_words_scores)))
+    return list(map(list, zip(map(parse_tokens_to_method_name, raw_prediction.topk_predicted_words),
+                              raw_prediction.topk_predicted_words_scores)))
 
 
-def parseTokensToMethodName(tokens: str) -> str:
-    methodName = ""
+def parse_tokens_to_method_name(tokens: str) -> str:
+    method_name = ""
     for i, token in enumerate(tokens.split("|")):
         if token and i:
             chars = list(token)
             chars[0] = chars[0].upper()
             token = "".join(chars)
-        methodName += token
-    return methodName
+        method_name += token
+    return method_name
 
 
-def padInput(input: str, config: Config) -> str:
-    return input + " " * (config.MAX_CONTEXTS - input.count(" "))
+def pad_input(inp: str, conf: Config) -> str:
+    return inp + " " * (conf.MAX_CONTEXTS - inp.count(" "))
 
-def shortenInput(input: str, config: Config) -> str:
-    split = input.split(" ")
+
+def shorten_input(inp: str, conf: Config) -> str:
+    split = inp.split(" ")
     random.shuffle(split)
-    return " ".join(split[:min(config.MAX_CONTEXTS + 1, len(split))])
+    return " ".join(split[:min(conf.MAX_CONTEXTS + 1, len(split))])
+
 
 if __name__ == "__main__":
     config = Config(set_defaults=True, load_from_args=True, verify=True)
@@ -46,5 +48,5 @@ if __name__ == "__main__":
     model = load_model(config)
 
     while True:
-        raw_input = shortenInput(padInput(input(), config), config)
+        raw_input = shorten_input(pad_input(input(), config), config)
         predict(model, raw_input)
